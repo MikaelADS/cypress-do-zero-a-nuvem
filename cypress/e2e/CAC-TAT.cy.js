fea4b96230d964833b7054b57b2b772a03e86bb7
MikaelADS <mikael205@mail.com>
2025-08-16 10:30:45 -0300

    Exercicio 7.2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -66,8 +66,16 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .click()
     cy.get('.error').should('be.visible')
   })
-  it('envia o formuário com sucesso usando um comando customizado', () => {
-    cy.fillMandatoryFieldsAndSubmit()
+  it.only('envia o formuário com sucesso usando um comando customizado', () => {
+    const data = {
+      firstName: 'janaina',
+      lastName: 'Braga',
+      email: 'mikael@gmail.com',
+      text: 'Teste.'
+    }
+
+    cy.fillMandatoryFieldsAndSubmit(data)
+    cy.get('.success').should('be.visible')
   })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,10 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
-    cy.get('#firstName').type("Mikael")
-    cy.get('#lastName').type("Braga")
-    cy.get('#email').type("mikael205@gmail.com")
-    cy.get('#open-text-area').type("Testando meu testemhfdhjskhfkdf fdskfhdjshk fdsfdsdfsdf")
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', data => {
+    cy.get('#firstName').type(data.firstName)
+    cy.get('#lastName').type(data.lastName)
+    cy.get('#email').type(data.email)
+    cy.get('#open-text-area').type(data.text)
     cy.contains('button', 'Enviar')
       .click()
-
-    cy.get('.success').should('be.visible')
-
 } )
 
 
